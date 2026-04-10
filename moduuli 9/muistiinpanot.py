e0aa0463870bdd578962e67e0780ebbce791842e
enjab <enjabehm@gmail.com>
2026-04-10 16:52:36 +0300

    tehtävät

--- moduuli 9/muistiinpanot.py
+++ moduuli 9/muistiinpanot.py
@@ -11,3 +11,32 @@ class Koira:
 
 k = Koira("Musti", 3)
 
+#Useampi olio + lista
+
+#Luo lista opiskelijoista
+#Käy lista läpi for-silmukalla
+#Tulosta kaikkien tiedot
+
+#Luodaan luokka opiskelija
+class Opiskelija:
+    def __init__(self, nimi, ika, kurssi):
+        self.nimi = nimi
+        self.ika = ika
+        self.kurssi = kurssi
+
+    def tulosta_tiedot(self):
+        print(f"Nimi: {self.nimi}, Ika: {self.ika}, Kurssi: {self.kurssi}")
+
+#Lista opiskleijaolioista
+
+opiskelijat = [
+    Opiskelija("Enja", 19, "Ohjelmointi"),
+    Opiskelija("Jere", 20,"Fysiikka"),
+    Opiskelija("Kari", 8, "Tietokannat")
+
+]
+
+#Käydään lista läpi for-silmukalla ja tulostetaan
+
+for opiskelija in opiskelijat:
+    opiskelija.tulosta_tiedot()
